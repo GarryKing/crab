a584f564a873b3cfe7f2c6d47384384bb9c935d6
GarryKing <hezeqing001@gmail.com>
2013-12-11 21:31:35 +0800

    routes优化

--- routes/home.js
+++ routes/home.js
@@ -1,21 +1,6 @@
 /**
  * Created by mao on 13-12-3.
  */
-
-var PicManager = require('../models/manager/PicManager');
-
-exports.loadPictures = function (req, res) {
-    var start = req.query.start;
-    var size = req.query.size;
-    //TODO 校验两个入参
-    PicManager.loadAllPictures(start, size, function (rs) {
-        var result = "";
-        if (rs != undefined && rs != null && rs.length > 0)
-            result = JSON.stringify(rs);
-        res.end(result);
-    })
-};
-
 exports.index = function (req, res) {
     res.render('index');
 };
