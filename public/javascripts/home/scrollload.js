6a46179f8046f4ee9ec76e22cc67a212c702c5f7
crabmore <crabmore@gmail.com>
2013-12-12 22:19:45 +0800

    延时加载参数调优

--- public/javascripts/home/scrollload.js
+++ public/javascripts/home/scrollload.js
@@ -9,9 +9,9 @@ function loadData() {
     toTop = parseFloat($(window).scrollTop());
     totalheight = parseFloat($(window).height()) + toTop;
     console.log("winH=" + $(document).height() * 0.8 + " , toTop=" + toTop);
-    if ($(document).height() * 0.8 <= toTop) {
-        loadImages(curr, pageSize)
-        curr = curr + pageSize
+    if ($(document).height() * 0.75 <= toTop) {
+        loadImages(curr, pageSize);
+        curr = curr + pageSize;
     }
 }
 $(window).scroll(function () {
